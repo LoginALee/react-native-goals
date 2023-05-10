d354b71edf8ff99a6f944664df45b360152024bd
Alejandro Tinoco Duarte <atinoco@innomius.com>
2023-05-10 13:39:44 -0600

    feature: Add Modal and styles

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import { FlatList, StyleSheet, View } from 'react-native'
+import { FlatList, StyleSheet, View, Button } from 'react-native'
 import GoalItem from './components/GoalItem'
 import GoalInput from './components/GoalInput'
 
 export default function App() {
   const [goals, setGoals] = useState([])
   const [currentGoal, setCurrentGoal] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const updateGoal = (e) => {
     setCurrentGoal(e.nativeEvent.text)
@@ -13,16 +14,27 @@ export default function App() {
 
   const addGoal = () => {
     setGoals([...goals, { text: currentGoal, id: Math.random().toString() }])
+    setCurrentGoal('')
+    closeModal()
   }
 
   const deleteGoal = (id) => setGoals(goals.filter((goal) => goal.id !== id))
 
+  const closeModal = () => setIsModalOpen(false)
+
+  const openModal = () => setIsModalOpen(true)
+
   return (
     <View style={styles.appContainer}>
+      <View style={styles.button}>
+        <Button color="white" title="Add new goal!" onPress={openModal} />
+      </View>
       <GoalInput
         addGoal={addGoal}
         updateGoal={updateGoal}
         currentGoal={currentGoal}
+        isModalOpen={isModalOpen}
+        closeModal={closeModal}
       />
       <View style={styles.goalsList}>
         <FlatList
@@ -41,7 +53,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    marginTop: 60,
+    marginTop: 80,
     marginHorizontal: 40,
     flexDirection: 'column',
     flex: 1,
@@ -49,5 +61,8 @@ const styles = StyleSheet.create({
   goalsList: {
     marginVertical: 50,
     flex: 12,
+  },
+  button: {
+    backgroundColor: 'violet',
   },
 })
--- components/GoalInput.jsx
+++ components/GoalInput.jsx
@@ -1,29 +1,62 @@
-import React, { useState } from 'react'
-import { StyleSheet, View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Modal, Image } from 'react-native'
 
-export default function GoalInput({ addGoal, updateGoal, currentGoal }) {
+export default function GoalInput({
+  addGoal,
+  updateGoal,
+  currentGoal,
+  closeModal,
+  isModalOpen,
+}) {
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Enter your goal!"
-        value={currentGoal}
-        onChange={updateGoal}
-      ></TextInput>
-      <View style={styles.addGoalBtn}>
-        <Button color="white" title="Add goal" onPress={addGoal} />
+    <Modal visible={isModalOpen} animationType="slide">
+      <View style={styles.mainContainer}>
+        <Image
+          style={styles.goalsIcon}
+          source={require('../assets/images/goals.png')}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your goal!"
+          value={currentGoal}
+          onChange={updateGoal}
+        ></TextInput>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.button}>
+            <Button color="white" title="Add goal" onPress={addGoal} />
+          </View>
+          <View style={styles.button}>
+            <Button color="white" title="Cancel" onPress={() => closeModal()} />
+          </View>
+        </View>
       </View>
-    </View>
+    </Modal>
   )
 }
 
 const styles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  mainContainer: {
+    backgroundColor: '#AD44B7',
+    justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
   },
-  addGoalBtn: {
+  input: {
+    width: '60%',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    padding: 10,
+  },
+  buttonsContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    marginHorizontal: 10,
+    paddingHorizontal: 24,
     backgroundColor: 'violet',
+  },
+  goalsIcon: {
+    height: 100,
+    width: 100,
+    marginBottom: 30,
   },
 })
